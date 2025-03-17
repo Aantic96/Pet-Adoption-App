163608cb0ce1192cacd19a1c32d8155d1fd129da
Antonela <gemory.mei@gmail.com>
2025-03-17 13:48:48 +0100

    Initial commit

---
+++ src/main/java/com/example/pet_adoption/model/Pet.java
@@ -0,0 +1,78 @@
+package com.example.pet_adoption.model;
+
+import java.sql.Date;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+
+@Entity
+@Table(name = "pets")
+public class Pet {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank
+    private String animal;
+    
+    @NotBlank
+    private String name;
+
+    private String breed;
+    private Date dateOfBirth;
+    private Boolean adoptionStatus = false;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+     public String getAnimal() {
+        return animal;
+    }
+
+    public void setAnimal(String animal) {
+        this.animal = animal;
+    }
+    
+    public String getBreed() {
+        return breed;
+    }
+
+    public void setBreed(String breed) {
+        this.breed = breed;
+    }
+
+    public Date getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public void setDateOfBirth(Date dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    public Boolean getAdoptionStatus() {
+        return adoptionStatus;
+    }
+
+    public void setBreed(Boolean adoptionStatus) {
+        this.adoptionStatus = adoptionStatus;
+    }
+
+}
